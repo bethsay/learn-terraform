863dacf66ad1aa19b76969784f34f08cf4ae801d
Bethsay Tom V <bethsay.tv@gmail.com>
2025-10-17 11:07:51 +0530

    Used the count meta-argument and the -target flag

--- basics/resources.tf
+++ basics/resources.tf
@@ -9,3 +9,9 @@ resource "local_sensitive_file" "sensitive_resource" {
   content  = "..Do not see these contents..\n"
   filename = "${path.module}/training/sensitive_file_by_tf.txt"
 }
+
+resource "local_file" "another_resource" {
+  content  = "...Hello again World${count.index}...!!\n"
+  filename = "${path.module}/training/another${count.index}_file_by_tf.txt"
+  count = 5
+}
