dc7da15ce63357ff9ff8372ef9e08c9b8f1fb94f
Bethsay Tom V <bethsay.tv@gmail.com>
2025-10-29 22:09:02 +0530

    Terraform output and console were checked, for loops and many functions have been explored

--- variables/output.tf
+++ variables/output.tf
@@ -1,11 +1,15 @@
 output "path" {
-  value = "All your files are created in ${local.path}"
+  # value = "They are all created and stored in ${local.path}"
+  value = "They are all created and stored in ${path.cwd}${trimprefix(local.path, ".")}"
   sensitive = false
 }
-output "item" {
-  value = "This is one item ${resource.local_file.file1[0].filename}"
+output "items" {
+  # value = format("Some of these items are %s", join(", ",resource.local_file.file1[*].filename))
+  value = format("Some of these items are %s", join(", ",[for attr in resource.local_file.file1 : basename(attr.filename)]))
+  # value = format("Some of these items are %s", join(", ",[for attr in resource.local_file.file1 : trimprefix(attr.filename,"${local.path}/")]))
+  # value = format("Some of these items are %s", join(", ",[for attr in resource.local_file.file1 : regex("([^/]+)$",attr.filename)[0]]))
+  # value = format("Some of these items are %s", join(", ",[for attr in resource.local_file.file1 : element(split("/",attr.filename),-1)]))
 }
-# output "count" {
-#   count = 
-#   value = ""
-# }
+output "count" {
+  value = format("We have %d files created", length(resource.local_file.file1) + length(resource.local_file.file2) + length(resource.local_file.env_files_set) + length(resource.local_file.component_files) + length(resource.local_file.env_files_list) + length(resource.local_file.content_files))
+}
