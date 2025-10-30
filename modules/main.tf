60dae9ed4a920d604341aecc8c6c5075784bfb2d
Bethsay Tom V <bethsay.tv@gmail.com>
2025-10-30 18:41:21 +0530

    Terraform modules were explored by creating a child module and using it in a root module

---
+++ modules/main.tf
@@ -0,0 +1,17 @@
+module "file-gen" {
+  source = "./child_module"
+  env = ["dev","stage","qa","edge","demo","prod","dr"]
+  component = {
+    "client" = "Welcome client. You will recieve data from frontend and you can repond to it"
+    "frontend" = "Client has connected. Serve content. Forward responses to backend"
+    "backend" = "Data from frontend will be processed here"
+  }
+}
+
+output "file-gen"{
+  value = {
+    count = module.file-gen.count
+    path = module.file-gen.path
+    items = module.file-gen.items
+  }
+}
